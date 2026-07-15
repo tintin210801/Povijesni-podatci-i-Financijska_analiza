496ba869efaacbde82a4b624ed617d9e840b8500
tintin210801 <tin.loncarevic@gmail.com>
2026-07-15 23:31:59 +0200

    Add script to download and process historical asset data
    This script downloads historical data for selected tickers and processes their prices and volumes.

---
+++ Nabava_podataka.py
@@ -0,0 +1,19 @@
+#Povijesni podatci i njihovo uređenje
+#Preuzimanje i obrada cijena nekoliko imovina te izrada čitljivih statističkih izvještaja
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from datetime import datetime
+Tickers = ['BTC-USD', 'ETH-USD', 'SPY', 'GOOGL', 'ASML', 'TSM']
+Data = yf.download(Tickers, start='2020-01-01', end= datetime.now())
+Price = Data["Close"].ffill()
+Price = Price.bfill()
+Volume =Data["Volume"]
+High = Data["High"]
+Low = Data["Low"]
+Price_btc =  Price["BTC-USD"]
+Price_eth = Price["ETH-USD"]
+Price_spy = Price["SPY"]
+Price_googl = Price["GOOGL"]
+Price_asml = Price["ASML"]
+Price_tsm = Price["TSM"]
