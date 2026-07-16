c9a62e2b249f24fff845810c6d353e1d94621b6c
tintin210801 <tin.loncarevic@gmail.com>
2026-07-16 21:23:28 +0200

    Create Struktura_podataka.py

---
+++ Struktura_podataka.py
@@ -0,0 +1,28 @@
+# Priprema podataka
+Data_shape = Data.shape
+Data_columns = Data.columns
+Data_dtypes = Data.dtypes
+Data_head = Data.head(5)
+Data_tail = Data.tail(5) 
+print("=" * 60)
+print("                  PREGLED STRUKTURE PODATAKA")
+print("=" * 60)
+
+print(f"DIMENZIJE: {Data_shape[0]} redova, {Data_shape[1]} stupaca")
+print("-" * 60)
+
+print("STUPCI:")
+print(list(Data_columns))
+print("-" * 60)
+
+print("TIPOVI PODATAKA (Dtypes):")
+print(Data_dtypes)
+print("-" * 60)
+
+print("PRVIH 5 REDOVA:")
+print(Data_head)
+print("-" * 60)
+
+print("ZADNJIH 5 REDOVA:")
+print(Data_tail)
+print("=" * 60)
