9004f356d6a7dbe0d909410aafa0bb9e9270dc81
tintin210801 <tin.loncarevic@gmail.com>
2026-07-16 22:38:39 +0200

    Update Nabava_podataka.py

--- Nabava_podataka.py
+++ Nabava_podataka.py
@@ -1,8 +1,5 @@
 #Povijesni podatci i njihovo uređenje
 #Preuzimanje i obrada cijena nekoliko imovina te izrada čitljivih statističkih izvještaja
-import yfinance as yf
-import numpy as np
-import pandas as pd
 from datetime import datetime
 Tickers = ['BTC-USD', 'ETH-USD', 'SPY', 'GOOGL', 'ASML', 'TSM']
 Data = yf.download(Tickers, start='2020-01-01', end= datetime.now())
