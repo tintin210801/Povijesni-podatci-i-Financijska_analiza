56d6cef6da76b247c3a00c94985b960f08d863c7
tintin210801 <tin.loncarevic@gmail.com>
2026-07-16 22:42:41 +0200

    Update Nabava_podataka.py

--- Nabava_podataka.py
+++ Nabava_podataka.py
@@ -1,6 +1,5 @@
 #Povijesni podatci i njihovo uređenje
 #Preuzimanje i obrada cijena nekoliko imovina te izrada čitljivih statističkih izvještaja
-from datetime import datetime
 Tickers = ['BTC-USD', 'ETH-USD', 'SPY', 'GOOGL', 'ASML', 'TSM']
 Data = yf.download(Tickers, start='2020-01-01', end= datetime.now())
 Price = Data["Close"].ffill()
